bdd27d97fb13bb78792c19d50b4c60071df49c5e
morshedulmunna <morshedulmunna1@gmail.com>
2024-07-10 18:07:03 +0600

    log response modifiy

--- libs/error.js
+++ libs/error.js
@@ -14,7 +14,25 @@ const globalErrorHandler = (err, req, res, next) => {
   err.statusCode = err.statusCode || 500;
   err.status = err.status || "error";
 
+<<<<<<< Updated upstream
   // Operational, trusted error: send message to client
+=======
+  // Create a unique error ID
+  const errorId = new Date().getTime();
+
+  // Log error details to the error log file
+  const errorLogDetails = `
+    Error ID: ${errorId}
+    Status: ${err.status}
+    StatusCode: ${err.statusCode}
+    URL: ${req.originalUrl}
+    Message: ${err.message}
+    Timestamp: ${new Date().toISOString()}
+  `;
+
+  errorLogStream.write(errorLogDetails);
+
+>>>>>>> Stashed changes
   if (err.isOperational) {
     res.status(err.statusCode).json({
       status: err.status,
