132e10a2bc0b5a5099b73c382f153f4ed4707574
morshedulmunna <morshedulmunna1@gmail.com>
2024-07-10 18:09:08 +0600

    cc

--- libs/error.js
+++ libs/error.js
@@ -16,12 +16,6 @@ const globalErrorHandler = (err, req, res, next) => {
   err.statusCode = err.statusCode || 500;
   err.status = err.status || "error";
 
-<<<<<<< HEAD
-<<<<<<< Updated upstream
-  // Operational, trusted error: send message to client
-=======
-=======
->>>>>>> 3b6ffd44a9f6981f8d1f0e3cc27c2814cdc25dc5
   // Create a unique error ID
   const errorId = new Date().getTime();
 
@@ -32,19 +26,12 @@ const globalErrorHandler = (err, req, res, next) => {
     StatusCode: ${err.statusCode}
     URL: ${req.originalUrl}
     Message: ${err.message}
-<<<<<<< HEAD
-=======
-    Stack: ${err.stack}
->>>>>>> 3b6ffd44a9f6981f8d1f0e3cc27c2814cdc25dc5
+    //Stack: ${err.stack}
     Timestamp: ${new Date().toISOString()}
   `;
 
   errorLogStream.write(errorLogDetails);
 
-<<<<<<< HEAD
->>>>>>> Stashed changes
-=======
->>>>>>> 3b6ffd44a9f6981f8d1f0e3cc27c2814cdc25dc5
   if (err.isOperational) {
     res.status(err.statusCode).json({
       status: err.status,
