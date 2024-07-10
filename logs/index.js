eb83705d6e3bd308993abda5c4b02637e6a49395
morshedulmunna <morshedulmunna1@gmail.com>
2024-07-10 18:57:27 +0600

    cc

--- logs/index.js
+++ logs/index.js
@@ -1,11 +1,8 @@
 const fs = require("fs");
 const path = require("path");
 
-const errorLogStream = fs.createWriteStream(
-  path.join(__dirname, "error.json"),
-  {
-    flags: "a",
-  }
-);
+const errorLogStream = fs.createWriteStream(path.join(__dirname, "error.log"), {
+  flags: "a",
+});
 
 module.exports = errorLogStream;
