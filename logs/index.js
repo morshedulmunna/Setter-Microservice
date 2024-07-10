965f3ea6da78ec5d58c0cc70f7d51a49eecacb2c
morshedulmunna <morshedulmunna1@gmail.com>
2024-07-10 18:19:19 +0600

    error log generate json file

--- logs/index.js
+++ logs/index.js
@@ -1,8 +1,11 @@
 const fs = require("fs");
 const path = require("path");
 
-const errorLogStream = fs.createWriteStream(path.join(__dirname, "error.log"), {
-  flags: "a",
-});
+const errorLogStream = fs.createWriteStream(
+  path.join(__dirname, "error.json"),
+  {
+    flags: "a",
+  }
+);
 
 module.exports = errorLogStream;
