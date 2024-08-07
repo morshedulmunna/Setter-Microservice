2043368eebd4cde51524e615f90260ab15745a7f
morshedulmunna <morshedulmunna1@gmail.com>
2024-08-07 14:33:22 +0600

    cc

---
+++ services/topProductService.js
@@ -0,0 +1,25 @@
+const { BACKEND_HOST } = require("../constant");
+const { AppError } = require("../libs/error");
+
+// Create a new hero
+exports.createCompanyService = async (req, res) => {
+  try {
+    const { tittle, description, features } = req.body;
+    const icon = req.file;
+
+    // Check for missing fields before processing
+    if (!icon || !tittle || !description) {
+      return res.status(400).json({ message: "All fields are required." });
+    }
+
+    const newService = new CompanyService({
+      icon: `${BACKEND_HOST}/uploads/service/${icon.filename}`,
+      tittle,
+      description,
+    });
+    await newService.save();
+    res.status(201).json(newService);
+  } catch (error) {
+    throw new AppError(error.message, 400);
+  }
+};
